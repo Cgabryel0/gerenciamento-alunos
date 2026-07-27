bfc9b2e704ae2aed02bc91737c24c059646985d9
Cgabryel0 <cgabryel0@gmail.com>
2026-07-27 01:04:15 -0300

    test: implementa testes de integracao dos controllers
    Implementa quatro testes distintos em AlunoControllerTest e UsuarioControllerTest usando MockMvc, totalizando 8 testes de integracao focados no comportamento dos controladores.
    
    Cobre os cenarios obrigatorios do enunciado: POST /pesquisar-aluno com nome vazio retornando a listagem completa sem erros, e POST /login com credenciais invalidas que nao redireciona para /index.

--- src/test/java/br/com/gerenciamento/controller/AlunoControllerTest.java
+++ src/test/java/br/com/gerenciamento/controller/AlunoControllerTest.java
@@ -1,10 +1,86 @@
 package br.com.gerenciamento.controller;
 
+import br.com.gerenciamento.enums.Curso;
+import br.com.gerenciamento.enums.Status;
+import br.com.gerenciamento.enums.Turno;
+import br.com.gerenciamento.model.Aluno;
+import br.com.gerenciamento.repository.AlunoRepository;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
 public class AlunoControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    private void gravarAluno(String nome) {
+        Aluno aluno = new Aluno();
+        aluno.setNome(nome);
+        aluno.setMatricula("MAT-" + UUID.randomUUID());
+        aluno.setCurso(Curso.CONTABILIDADE);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setTurno(Turno.NOTURNO);
+        alunoRepository.save(aluno);
+    }
+
+    @Test
+    public void pesquisaComNomeVazioDeveTrazerTodosOsAlunos() throws Exception {
+        gravarAluno("Ana Paula Souza");
+        gravarAluno("Bruno Carvalho");
+
+        mockMvc.perform(post("/pesquisar-aluno").param("nome", ""))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("ListaDeAlunos"))
+                .andExpect(model().attribute("ListaDeAlunos", hasSize(greaterThanOrEqualTo(2))));
+    }
+
+    @Test
+    public void pesquisaSemInformarNomeDeveTrazerTodosOsAlunos() throws Exception {
+        gravarAluno("Joana Ribeiro");
+
+        mockMvc.perform(post("/pesquisar-aluno"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("ListaDeAlunos"));
+    }
+
+    @Test
+    public void pesquisaPorNomeDeveFiltrarOsResultados() throws Exception {
+        String token = UUID.randomUUID().toString().substring(0, 6);
+        gravarAluno("Cliente " + token);
+        gravarAluno("Outro Aluno");
+
+        mockMvc.perform(post("/pesquisar-aluno").param("nome", token))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("ListaDeAlunos", hasSize(1)));
+    }
+
+    @Test
+    public void pesquisaPorNomeInexistenteDeveTrazerListaVazia() throws Exception {
+        gravarAluno("Pedro Henrique");
+
+        mockMvc.perform(post("/pesquisar-aluno").param("nome", "zzz-" + UUID.randomUUID()))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("ListaDeAlunos", hasSize(0)));
+    }
 }
--- src/test/java/br/com/gerenciamento/controller/UsuarioControllerTest.java
+++ src/test/java/br/com/gerenciamento/controller/UsuarioControllerTest.java
@@ -1,10 +1,81 @@
 package br.com.gerenciamento.controller;
 
+import br.com.gerenciamento.model.Usuario;
+import br.com.gerenciamento.repository.UsuarioRepository;
+import br.com.gerenciamento.util.Util;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
 public class UsuarioControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    private String sufixo() {
+        return UUID.randomUUID().toString().substring(0, 8);
+    }
+
+    @Test
+    public void loginComCredenciaisInvalidasDeveApresentarTelaDeCadastro() throws Exception {
+        mockMvc.perform(post("/login")
+                        .param("user", "ghost_" + sufixo())
+                        .param("senha", "000"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login/cadastro"));
+    }
+
+    @Test
+    public void loginComCredenciaisValidasDeveAbrirAHome() throws Exception {
+        String login = "acesso_" + sufixo();
+        Usuario usuario = new Usuario();
+        usuario.setEmail(login + "@mail.com");
+        usuario.setUser(login);
+        usuario.setSenha(Util.md5("123"));
+        usuarioRepository.save(usuario);
+
+        mockMvc.perform(post("/login")
+                        .param("user", login)
+                        .param("senha", "123"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home/index"));
+    }
+
+    @Test
+    public void cadastroDeUsuarioDeveRedirecionarParaRaiz() throws Exception {
+        String marca = sufixo();
+
+        mockMvc.perform(post("/salvarUsuario")
+                        .param("user", "novo_" + marca)
+                        .param("email", "novo" + marca + "@mail.com")
+                        .param("senha", "123"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"));
+    }
+
+    @Test
+    public void logoutDeveRetornarParaTelaDeLogin() throws Exception {
+        mockMvc.perform(post("/logout"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login/login"));
+    }
 }
